112d1a1bf9858ff50ba909266a98b64641ae7fa1
kinglemuel <pro@kinglemuel.com>
2016-06-12 14:05:40 -0400

    finish views

--- snippets/views.py
+++ snippets/views.py
@@ -1,47 +1,66 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetList(mixins.ListModelMixin,
-				  mixins.CreateModelMixin,
-				  generics.GenericAPIView):
+
+class SnippetList(generics.ListCreateAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
-
-
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-
-
-
-class SnippetDetail(mixins.RetrieveModelMixin, 
-					mixins.UpdateModelMixin, 
-					mixins.DestroyModelMixin, 
-					generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-
-	def destroy(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
 
 
-
+# ---------------------------------- Mixins and Generics -----------------
 
 
 # from snippets.models import Snippet
 # from snippets.serializers import SnippetSerializer
-# from django.http import Http404 
+# from rest_framework import mixins
+# from rest_framework import generics
+
+# class SnippetList(mixins.ListModelMixin,
+# 				  mixins.CreateModelMixin,
+# 				  generics.GenericAPIView):
+# 	queryset = Snippet.objects.all()
+# 	serializer_class = SnippetSerializer
+
+
+# 	def get(self, request, *args, **kwargs):
+# 		return self.list(request, *args, **kwargs)
+
+
+# 	def post(self, request, *args, **kwargs):
+# 		return self.create(request, *args, **kwargs)
+
+
+
+# class SnippetDetail(mixins.RetrieveModelMixin,
+# 					mixins.UpdateModelMixin,
+# 					mixins.DestroyModelMixin,
+# 					generics.GenericAPIView):
+# 	queryset = Snippet.objects.all()
+# 	serializer_class = SnippetSerializer
+
+# 	def get(self, request, *args, **kwargs):
+# 		return self.retrieve(request, *args, **kwargs)
+
+# 	def put(self, request, *args, **kwargs):
+# 		return self.update(request, *args, **kwargs)
+
+# 	def destroy(self, request, *args, **kwargs):
+# 		return self.destroy(request, *args, **kwargs)
+
+
+
+#------------------------------------ No Mixins nor Generics ----------------
+
+# from snippets.models import Snippet
+# from snippets.serializers import SnippetSerializer
+# from django.http import Http404
 # from rest_framework import status
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
@@ -101,4 +120,3 @@ class SnippetDetail(mixins.RetrieveModelMixin,
 # 		snippet = self.get_object(pk)
 # 		snippet.delete()
 # 		return Response(status=status.HTTP_204_NO_CONTENT)
-
